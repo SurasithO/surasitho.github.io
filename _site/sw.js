c243c68bd2b061c6d3f686ab928555ef71d2d4c1
Surasith Boonaneksap <47601832+SurasithO@users.noreply.github.com>
2022-08-24 16:18:11 +0700

    Test self-hosted assets option

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220824.160643';
+const cacheName = 'chirpy-20220824.161627';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
