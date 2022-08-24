f9f4f42eb0d2b89f7fe4b9d5e4301e5307005c71
Surasith Boonaneksap <47601832+SurasithO@users.noreply.github.com>
2022-08-24 16:08:27 +0700

    Update Social Icons

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220823.224644';
+const cacheName = 'chirpy-20220824.160643';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
