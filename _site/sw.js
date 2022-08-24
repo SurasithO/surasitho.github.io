7bfba9eb6c6a9c3d2460da83bbb4a212b4867174
Surasith Boonaneksap <47601832+SurasithO@users.noreply.github.com>
2022-08-24 17:54:12 +0700

    Update LinkedIn link
    Final update before switching to Github Desktop

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220824.162540';
+const cacheName = 'chirpy-20220824.174244';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
